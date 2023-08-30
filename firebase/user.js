eb15fca354ccd669e53890cbc7182ab18cdeff52
Aaryan Pawar <73911296+EliteCoder21@users.noreply.github.com>
2023-08-30 16:59:10 -0700

    Worked on firebase backend for kanban

--- firebase/user.js
+++ firebase/user.js
@@ -67,21 +67,8 @@ export async function getUserHoldings(id) {
 }
 
 export async function getUserAssets(id) {
-  try {
-    const docRef = doc(db, "assets", id);
-    const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      const data = docSnap.data();
-      return data;
-    } else {
-      console.log("No such document!");
-      return null;
-    }
-  } catch (error) {
-    console.error("Error getting document:", error);
-    return null;
-  }
+  return doc(collection(db, "assets"), id);
 }
 
 export async function addUserHoldings(id, data) {
--- pages/assets.js
+++ pages/assets.js
@@ -1,43 +1,56 @@
 import Navbar from "../components/navbar.js";
-import { useAuthContext } from '../firebase/context';
 import Login from "../components/login.js";
 import AsyncBoard from 'react-trello';
+import { useAuthContext } from "../firebase/context";
 import React, { useEffect, useState } from 'react';
+import { addUserActivity, getUserAssets } from "../firebase/user.js";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Assets() {
 
-  async function getActivityData() {
-    try {
-      const docsSnap = await getUserActivities(user.uid);
-      const userData = docsSnap.data();
+  // Essential Variables
+  const { user } = useAuthContext();
+  const data = require('./kanbanTestData.json')
+  let eventBus = null;
+  const setEventBus = (handle) => {
+    eventBus = handle;
+  }
 
-      docsSnap.forEach((doc) => {
+  async function getAssetsData() {
+    try {
+      const userDataRef = await getUserAssets("5ntPFGMhxD4llc0ObTwF"); //Replace with user.uid
+      let oxaData = getDocs(collection(userDataRef, 'OXA'));
+      let digitalData = getDocs(collection(userDataRef, 'Digital Assets'));
+      let autData = getDocs(collection(userDataRef, 'AUT'));
+      let rwaData = getDocs(collection(userDataRef, 'RWA'));
+      
+      // Flush each of the data files
+      data.lanes[0].cards = [];
+      data.lanes[1].cards = [];
+      data.lanes[2].cards = [];
+      data.lanes[3].cards = [];
+
+      console.log(rwaData);
+
+      // Push the data
+      rwaData.forEach((doc) => {
         // Get the data
         const data = doc.data();
 
         // Append the data
-        TABLE_STATE.push({
-          date: data.date,
-          coin: data.coin,
-          amount: data.amount,
-          type: data.type,
-          notes: data.notes,
+        data.lanes[0].cards.push({
+          id: data.id,
+          laneId: data.laneId,
+          title: data.title,
+          label: data.label,
+          cardStyle: { "width": 380, "maxWidth": 380, "margin": "auto", "marginBottom": 5 },
+          description: data.description
         });
       });
 
-      setActivityData(TABLE_STATE);
-      setOriginalActivityData(TABLE_STATE);
     } catch (error) {
       console.log(error);
     }
-  }
-
-  // Essential Variables
-  const { user } = useAuthContext();
-  var data = require('./kanbanTestData.json')
-  let eventBus = null;
-  const setEventBus = (handle) => {
-    eventBus = handle;
   }
 
   // Find JSON data
@@ -58,9 +71,9 @@ export default function Assets() {
     console.log("end: ", toLaneId);
 
     // Firebase method 
+    getAssetsData("5ntPFGMhxD4llc0ObTwF"); //Replace with user.uid
 
-    console.log("Final Result:")
-    console.log(JSON.stringify(data));
+    
   }
 
   const onCardAdd = (card, laneId) => {
@@ -73,7 +86,6 @@ export default function Assets() {
       <div className='bond-data'>
         <div className="myAssets">
           <AsyncBoard
-            editable
             eventBusHandle={setEventBus} 
             style={{backgroundColor: 'rgba(31, 42, 71, 0)'}}
             data={data}
