226c38bf6fe8558aeaf39acd7ca1f858610d0da6
Shashvath-S <119080138+Shashvath-S@users.noreply.github.com>
2023-12-12 06:33:32 -0800

    Change some icons and update Dig lane

--- pages/assets.js
+++ pages/assets.js
@@ -18,6 +18,8 @@ import Tearsheet from "../components/tearsheet.js";
 import CryptoTearsheet from "../components/crypto-tearsheet.js";
 import AddIcon from "@mui/icons-material/Add";
 import RelValIcon from "@mui/icons-material/ScatterPlot";
+import WalletIcon from '@mui/icons-material/Wallet';
+import SmartButtonIcon from '@mui/icons-material/SmartButton';
 import TearSheetIcon from "@mui/icons-material/Summarize";
 import "reactjs-popup/dist/index.css";
 import Script from "next/script";
@@ -223,7 +225,7 @@ export default function Assets() {
                   setDisplayCryptoTearsheetPopup(!displayCryptoTearsheetPopup);
                 }}
               >
-                <TearSheetIcon />
+                <SmartButtonIcon />
               </button>
             </div>
           )}
@@ -233,17 +235,17 @@ export default function Assets() {
           <div className="react-trello-card-body">
             <p>{card.description}</p>
             <div className="progress">
-            <p class="leftProgressText">&#9680;</p>
-            <p class="rightProgressText">In Progress</p>
+            <p class="leftProgressText">&#9680; In Progress</p>
             </div>
           </div>
         ) : (
-          <div style={{width: "50%"}}>
-            <Script defer src="https://www.livecoinwatch.com/static/lcw-widget.js" /> 
-            <div style={{width: "25%"}} class="livecoinwatch-widget-6" lcw-coin="BTC" lcw-base="USD" lcw-period="d" lcw-color-tx="#ffffff" lcw-color-bg="#1f2434" lcw-border-w="1" ></div>
-            {/* <Script style={{width: "100%"}} defer src="https://www.livecoinwatch.com/static/lcw-widget.js" />
+          <>
+          <div className="react-trello-card-body">
+            {card.title == "BTC - Bitcoin" ? (
+              <>
+              <Script defer src="https://www.livecoinwatch.com/static/lcw-widget.js" />
               <div 
-                class="livecoinwatch-widget-1" 
+                class="livecoinwatch-widget-6" 
                 lcw-coin="BTC" 
                 lcw-base="USD" 
                 lcw-secondary="BTC" 
@@ -253,8 +255,16 @@ export default function Assets() {
                 lcw-color-bg="#1f2434" 
                 lcw-border-w="1"
               >
-              </div> */}
+              </div>
+              </>
+            ) : (
+            <>
+              <Script defer src="https://www.livecoinwatch.com/static/lcw-widget.js" /> <div class="livecoinwatch-widget-6" lcw-coin="ETH" lcw-base="USD" lcw-period="d" lcw-color-tx="#ffffff" lcw-color-bg="#1f2434" lcw-border-w="1" ></div>
+              </>
+            )}
+
           </div>
+        </>
         )};
       </div>
     );
@@ -277,7 +287,7 @@ export default function Assets() {
         res = "Drop here to get credit for AUT's"
         break;
       case "Dig Lane":
-        res = "Swap back to OXA/AUT/RWA"
+        res = "Convert back to OXA/AUT/RWA"
       default:
         break;
     }
@@ -297,24 +307,15 @@ export default function Assets() {
           >
             <AddIcon />
           </button>
-        ) : laneId == "AUT Lane" ? (
-          <div style={{marginTop: 60, marginRight: 5}}>
-            {/* <MultipleStopOutlinedIcon /> */}
-            <svg
-              width={30}
-              height={20}
-              className="m-auto"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 512 512"
-            >
-              <path d="M535 41c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l64 64c4.5 4.5 7 10.6 7 17s-2.5 12.5-7 17l-64 64c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l23-23L384 112c-13.3 0-24-10.7-24-24s10.7-24 24-24l174.1 0L535 41zM105 377l-23 23L256 400c13.3 0 24 10.7 24 24s-10.7 24-24 24L81.9 448l23 23c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L7 441c-4.5-4.5-7-10.6-7-17s2.5-12.5 7-17l64-64c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9zM96 64H337.9c-3.7 7.2-5.9 15.3-5.9 24c0 28.7 23.3 52 52 52l117.4 0c-4 17 .6 35.5 13.8 48.8c20.3 20.3 53.2 20.3 73.5 0L608 169.5V384c0 35.3-28.7 64-64 64H302.1c3.7-7.2 5.9-15.3 5.9-24c0-28.7-23.3-52-52-52l-117.4 0c4-17-.6-35.5-13.8-48.8c-20.3-20.3-53.2-20.3-73.5 0L32 342.5V128c0-35.3 28.7-64 64-64zm64 64H96v64c35.3 0 64-28.7 64-64zM544 320c-35.3 0-64 28.7-64 64h64V320zM320 352a96 96 0 1 0 0-192 96 96 0 1 0 0 192z" />
-            </svg>
-          </div>
         ) : (
-          <div style={{marginTop: 40, marginRight: 5}}>
-          {/* <MultipleStopOutlinedIcon /> */}
-            <svg
+          <div>
+            {laneId == "Dig Lane" ? (
+              <div style={{marginTop: 40}}>
+              <WalletIcon />
+              </div>
+            ) : (
+              <div style={{marginTop: 40, marginRight: 5}}>
+              <svg
               width={30}
               height={20}
               className="m-auto"
@@ -324,6 +325,8 @@ export default function Assets() {
             >
               <path d="M535 41c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l64 64c4.5 4.5 7 10.6 7 17s-2.5 12.5-7 17l-64 64c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l23-23L384 112c-13.3 0-24-10.7-24-24s10.7-24 24-24l174.1 0L535 41zM105 377l-23 23L256 400c13.3 0 24 10.7 24 24s-10.7 24-24 24L81.9 448l23 23c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L7 441c-4.5-4.5-7-10.6-7-17s2.5-12.5 7-17l64-64c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9zM96 64H337.9c-3.7 7.2-5.9 15.3-5.9 24c0 28.7 23.3 52 52 52l117.4 0c-4 17 .6 35.5 13.8 48.8c20.3 20.3 53.2 20.3 73.5 0L608 169.5V384c0 35.3-28.7 64-64 64H302.1c3.7-7.2 5.9-15.3 5.9-24c0-28.7-23.3-52-52-52l-117.4 0c4-17-.6-35.5-13.8-48.8c-20.3-20.3-53.2-20.3-73.5 0L32 342.5V128c0-35.3 28.7-64 64-64zm64 64H96v64c35.3 0 64-28.7 64-64zM544 320c-35.3 0-64 28.7-64 64h64V320zM320 352a96 96 0 1 0 0-192 96 96 0 1 0 0 192z" />
             </svg>
+            </div>
+            )}
           </div>
         )}
       </div>
@@ -353,7 +356,7 @@ export default function Assets() {
                 {getLaneImage(lane.id)}
               </center>
             </div>
-            {lane.id == "OXA2 Lane" ? (
+            {lane.id == "Dig Lane" ? (
               <div className="liquid-oxa-subtitle">
                 Liquid OXA:<br/><b>{numberWithCommas(liquidOxaAmount)}</b>
               </div>
@@ -386,7 +389,16 @@ export default function Assets() {
         </div>
         <div className="board-data-container">
           <div className="bond-data">
-            <div className="board-container">
+            <div
+              style={{
+                backgroundColor: "rgba(32, 34, 50, 0.55)",
+                borderRadius: 20,
+                width: "100%",
+                height: "70vh",
+                paddingBottom: 10,
+                overflowX: "scroll"
+              }}
+            >
               <Board
                 eventBusHandle={setEventBus}
                 style={{
