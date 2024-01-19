fd228c8dbe07a9bb82281bef55a99d95ef5d4ab2
Firouzja <96846293+Firouzja@users.noreply.github.com>
2024-01-18 17:57:29 -0800

    worked on kanban board

--- pages/assets.js
+++ pages/assets.js
@@ -245,28 +245,7 @@ export default function Assets() {
         ) : (
           <>
             <div className="react-trello-card-body">
-              {card.title == "BTC - Bitcoin" ? (
-                <>
-                  <Script defer src="https://www.livecoinwatch.com/static/lcw-widget.js" />
-                  <div
-                    class="livecoinwatch-widget-6"
-                    lcw-coin="BTC"
-                    lcw-base="USD"
-                    lcw-secondary="BTC"
-                    lcw-period="d"
-                    lcw-color-tx="#ffffff"
-                    lcw-color-pr="#58c7c5"
-                    lcw-color-bg="#1f2434"
-                    lcw-border-w="1"
-                  >
-                  </div>
-                </>
-              ) : (
-                <>
-                  <Script defer src="https://www.livecoinwatch.com/static/lcw-widget.js" /> <div class="livecoinwatch-widget-6" lcw-coin="ETH" lcw-base="USD" lcw-period="d" lcw-color-tx="#ffffff" lcw-color-bg="#1f2434" lcw-border-w="1" ></div>
-                </>
-              )}
-
+              <Script defer src="https://www.livecoinwatch.com/static/lcw-widget.js" />
             </div>
           </>
         )};
