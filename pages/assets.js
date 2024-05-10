54e36b994235277af64654bc520649debb6e8e50
Firouzja <96846293+Firouzja@users.noreply.github.com>
2024-05-09 17:56:57 -0700

    worked on kanban board

--- pages/assets.js
+++ pages/assets.js
@@ -1,4 +1,5 @@
 import Navbar from "../components/navbar.js";
+
 import Login from "../components/login.js";
 import Board from "react-trello";
 import { useAuthContext } from "../firebase/context";
